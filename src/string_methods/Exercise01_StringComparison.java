67bb0c05e3e5b4340d22d9878326435bb366a11e
Malek Shaar <malekshaar@gmail.com>
2023-01-11 14:56:27 -0600

    Hmwk 14

--- src/string_methods/Exercise01_StringComparison.java
+++ src/string_methods/Exercise01_StringComparison.java
@@ -8,13 +8,23 @@ public class Exercise01_StringComparison {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Str;");
+//        System.out.println("Enter Str;");
+//
+//        String st = input.nextLine();
+//
+//        System.out.println("Enter Str;");
+//
+//        String str2 = input.nextLine();
 
-        String st = input.nextLine();
+        String str = "kelaM";
+        int count = str.length();
+        String rev = "";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            rev = rev + str.charAt(i);
 
-        System.out.println("Enter Str;");
+            System.out.println(rev);
 
-        String str2 = input.nextLine();
+        }
 
     }
 }
